e5b487e561c8d8a6a3dd2bbe9dd7739d93c338b5
Jin Pan <jinxp1@gmail.com>
2014-11-22 15:53:53 -0500

    update admin

--- website/core/admin.py
+++ website/core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Doctor
+from core.models import Event
+from core.models import EventType
+from core.models import FamilyMember
+from core.models import Location
+from core.models import Nurse
+from core.models import Patient
+
+admin.site.register(Patient)
+admin.site.register(FamilyMember)
+admin.site.register(Doctor)
+admin.site.register(Nurse)
+
+admin.site.register(Event)
+admin.site.register(EventType)
+
+admin.site.register(Location)
+
