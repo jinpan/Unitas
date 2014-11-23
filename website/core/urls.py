3b5d1ad9f4a5bb360ff5fedbf9005ec58384c415
Erika Lu <erikalu@mit.edu>
2014-11-23 11:14:21 -0500

    Added get all doctors for dropdown list

--- website/core/urls.py
+++ website/core/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('core.views',
     url(r'^locations', 'get_locations'),
+    url(r'^doctors', 'get_doctors'),
     url(r'^flag_on', 'flag_on'),
     url(r'^flag_off', 'flag_off'),
     url(r'^events', 'get_events', name='get_events'),
     url(r'^patients', 'get_patients', name='get_patients')
-
 
 )
 
