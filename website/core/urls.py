039d028cd098a52a00836e250bf7267a71b30d63
Jin Pan <jinxp1@gmail.com>
2014-11-22 20:28:17 -0500

    update authentication

--- website/core/urls.py
+++ website/core/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('core.views',
     url(r'^locations', 'get_locations'),
-    url(r'^events', 'get_events'),
+    url(r'^events', 'get_events', name='get_events'),
 )
 
--- website/core/views.py
+++ website/core/views.py
@@ -1,16 +1,34 @@
 from json import dumps
 import datetime
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from core.models import Location
 from core.models import Event
+from core.models import Patient
+
+def login(request):
+    ssn = request.POST.get('ssn')
+
+    if ssn and Patient.objects.filter(SSN=ssn):
+        patient = Patient.objects.get(SSN=ssn)
+        user = patient.user
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        auth_login(request, user)
+        return redirect('get_events')
+
+    return render(request, 'registration/login2.html')
+
 
 def JSONResponse(data):
     return HttpResponse(dumps(data), content_type='application/json')
 
 
+@login_required
 def get_locations(request):
 
     locations = []
@@ -19,6 +37,7 @@ def get_locations(request):
 
     return JSONResponse(locations)
 
+@login_required(login_url='/accounts/login2')
 def get_events(request):
     # request will contain patient id, date
     p_id = request.GET.get('patient_id')
@@ -28,7 +47,6 @@ def get_events(request):
 
     start_dt = datetime.datetime.strptime(date, '%m/%d/%Y')
     end_dt = start_dt + datetime.timedelta(days=1) - datetime.timedelta(seconds=1)
-    print start_dt, end_dt
 
     events = Event.objects.filter(starttime__lte=end_dt, endtime__gte=start_dt, patient__pk=p_id)
 
